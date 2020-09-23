54472bacbda9a3ccb9b357b88d0806e6b3eda624
amandagxagxa <amandagxagxa@gmail.com>
2020-09-23 17:21:20 +0200

    exam prep

---
+++ src/ExamPrep/JavaBasics.java
@@ -0,0 +1,16 @@
+package ExamPrep;
+
+public class JavaBasics {
+
+//        public static void main(String[] args){
+////            System.out.println(args[1]);
+//
+//
+//    }
+
+//        public static long main(String[] args){
+//            System.out.println("Hello");
+//            return 10L;
+//        }
+
+}
---
+++ src/exam/stuff/Tea.java
@@ -0,0 +1,9 @@
+package exam.stuff;
+
+import cert.Beverage;
+
+ class Tea extends Beverage {
+    public static void main(String[] args) {
+
+    }
+}
